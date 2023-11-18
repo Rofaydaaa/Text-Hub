d280f4b7d1e04fe17198f4e4a2048ed6828d351f
Rofayda <roufybassem@gamil.com>
2023-11-18 03:35:44 +0200

    Adding update form and post form

--- texthub/forms.py
+++ texthub/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from texthub.models import User
+from flask_ckeditor import CKEditorField
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -29,4 +31,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already register on the system')
+        
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    body = CKEditorField('Body', validators=[DataRequired("Your text is empty, can't post it!")])
+    submit = SubmitField('Post')
     
